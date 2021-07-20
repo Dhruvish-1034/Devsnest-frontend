b95f5f3b290069d1d42a6eff2a74bd267b96aab3
WOLF0326 <86477621+WOLF0326@users.noreply.github.com>
2021-07-20 20:00:57 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Card from "./day 16/Card";
+import Card2 from "./day 16/Card2";
+import Calorie_card from "./day 17/Calorie_card";
+import Board from "./day 18/Board";
+import App from "./day 19/App";
+import Calorie_card1 from "./day 18/Calorie_card1";
+import Calorie_card2 from "./day 20/Calorie-card3";
+import "./index.css";
+
+// ReactDom.render(<Card />, document.getElementById("root"));
+// ReactDom.render(<Card2 />, document.getElementById("root"));
+// ReactDom.render(<Board />, document.getElementById("root"));
+// ReactDom.render(<Calorie_card />, document.getElementById("root"));
+// ReactDom.render(<App />, document.getElementById("root"));
+// ReactDom.render(<Calorie_card1 />, document.getElementById("root"));
+ReactDom.render(<Calorie_card2 />, document.getElementById("root"));
